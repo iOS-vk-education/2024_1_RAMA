b1e757335d790cd328dacd1adae1b059f55093e1
adev05 <a_nasibulin@rambler.ru>
2024-11-26 18:11:54 +0300

    feat: deadlinesView

---
+++ MAIApp/Deadlines/CalendarView.swift
@@ -0,0 +1,26 @@
+//
+//  DatePickerView.swift
+//  MAIApp
+//
+//  Created by Андрей  Насибулин  on 23.11.2024.
+//
+
+import SwiftUI
+
+struct DatePickerView: View {
+    @State private var date = Date()
+    
+    var body: some View {
+        DatePicker(
+            "Start Date",
+            selection: $date,
+            displayedComponents: [.date]
+        )
+        .environment(\.locale, Locale.init(identifier: "ru_RU"))
+        .datePickerStyle(.graphical)
+    }
+}
+
+#Preview {
+    DatePickerView()
+}
---
+++ MAIApp/Deadlines/DeadlinesView.swift
@@ -0,0 +1,18 @@
+//
+//  DeadlinesView.swift
+//  MAIApp
+//
+//  Created by Андрей  Насибулин  on 23.11.2024.
+//
+
+import SwiftUI
+
+struct DeadlinesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeadlinesView()
+}
